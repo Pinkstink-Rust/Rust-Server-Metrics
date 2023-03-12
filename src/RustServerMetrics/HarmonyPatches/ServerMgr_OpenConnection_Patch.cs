57260c848e5f16882aab6b949fe97886ebb23db5
xXTurner <40324267+xXTurnerLP@users.noreply.github.com>
2023-03-12 20:51:54 +0200

    add null check

--- src/RustServerMetrics/HarmonyPatches/ServerMgr_OpenConnection_Patch.cs
+++ src/RustServerMetrics/HarmonyPatches/ServerMgr_OpenConnection_Patch.cs
@@ -8,7 +8,7 @@ namespace RustServerMetrics.HarmonyPatches
         [HarmonyPostfix]
         public static void Postfix()
         {
-            MetricsLogger.Instance?.OnServerStarted();
+            MetricsLogger?.Instance?.OnServerStarted();
         }
     }
 }
